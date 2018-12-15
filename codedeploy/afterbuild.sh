6b88c2753f55a2991db4e561c0648754bf960e2c
stepweb-devops <45719433+stepweb-devops@users.noreply.github.com>
2018-12-15 20:57:50 +0530

    Update afterbuild.sh

--- codedeploy/afterbuild.sh
+++ codedeploy/afterbuild.sh
@@ -2,4 +2,4 @@
 buildpath=/home/sahil
 DATE=`date '+%Y-%m-%d %H:%M'`
 cd $buildpath && sudo apt-get install zip && sudo apt-get install unzip
-cd /var/www && sudo zip -r html.zip html && sudo mkdir -p /backup/$DATE && sudo cp -r html.zip /backup/$DATE
+cd /var/www && sudo zip -r html.zip html && sudo mkdir -p /backup/$DATE && sudo cp -r html.zip /backup/$DATE && sudo rm -rf /var/www/html.zip
