c45fce4b29a1d9ee379622b73043e74b0b640fc6
stepweb-devops <45719433+stepweb-devops@users.noreply.github.com>
2018-12-15 20:49:37 +0530

    Update build.sh

--- codedeploy/build.sh
+++ codedeploy/build.sh
@@ -3,4 +3,4 @@ buildpath=/home/sahil
 newbuild=/build/latest
 echo $PWD
 cd $buildpath && sudo npm install && sudo npm run build
-sudo rm -r -f $newbuild && sudo mkdir -p $newbuild && sudo cp -r $buildpath/build $newbuild
+sudo rm -r -f $newbuild && sudo mkdir -p $newbuild && sudo cp -r $buildpath/build/* $newbuild
