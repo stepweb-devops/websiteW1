3a5480bc8ce35f0172abd860721297cf9a0c7091
stepweb-devops <45719433+stepweb-devops@users.noreply.github.com>
2018-12-15 20:44:20 +0530

    Update beforebuild.sh

--- codedeploy/beforebuild.sh
+++ codedeploy/beforebuild.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 buildpath=/home/sahil
 echo "$PWD"
-cd $buildpath && sudo chmod -R 777 ../*
+cd $buildpath && sudo rm -rf build && sudo chmod -R 777 ../*
 #sudo npm rebuild node-sass && sudo npm i
